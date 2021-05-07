210adde4a2e0b0e536e893678bfa0867461c6128
baroudeColombbus <69351692+baroudeColombbus@users.noreply.github.com>
2021-05-07 21:34:13 +0200

    suite

--- inc/footer.php
+++ inc/footer.php
@@ -1,33 +1,79 @@
-<footer class="text-muted py-5">
-    <div class="container">
+<footer class="container-fluid text-muted py-5 bg-warning  ">
+    <div class="container container-sm">
         <div class="row ">
 
-            <div class="col-3">
-                <h6 class="text-upper text-dark">Rejoignez notre communauté</h6>
-                <input type="email" name="email" id="homemail" placeholder="E-MAIL">
+            <div class="col-lg-3  d-none d-md-block d-lg-block">
+                <div class="row">
+                   <div class="col">
+                        <img src="../img/m_MIXFOOD.png" alt="logo-mixfoo" class=" mixfooter align-center pt-5">
+                       
+                   </div>
+                </div>
             </div>
-            <div class="col-3">
-
-
+            <div class="col-lg-3  d-none d-md-block d-lg-block">
             </div>
-            <div class="col-2">
-                <h6 class="text-upper text-dark">Moyen de paiements</h6>
+            <div class=" col-6 col-lg-2">
+                <h6 class="text-upper text-dark">Moyen de paiementst</h6>
                 <P>Paypal</P>
                 <P>Espece</P>
                 <P>Carte bancaire</P>
             </div>
-            <div class="col-2">
+            <div class="col-6 col-lg-2">
                 <h6 class="text-upper text-dark">Contact</h6>
                 <p> Connexion</p>
                 <p> Menu</p>
                 <p> Service </p>
             </div>
             <div class="col">
-                <img src="../img/m_MIXFOOD.png" alt="logo-mixfoo" class="align-middle">
+                <div class="row">
+                    <p class="text-center text-md-left "><img src="../img/m_MIXFOOD.png" alt="logo-mixfoo" class=" align-center pt-5"></p>
+
+                </div>
 
             </div>
 
+        </div><!-- /row -->
+        <div class="row">
+            <p class="fw-light"> &copy;<?php echo date("Y");  ?> mixfood tous droits réservés </p>
         </div>
 
     </div>
+    <style>
+        /* Small devices (landscape phones, 576px and up) */
+        @media (min-width: 576px) {
+            .mixfooter p, h6 {
+                font-size: 12px;}
+
+                div .mixfooter  {
+
+                    min-width: 50%;
+
+            }
+
+
+         
+            }
+    
+            
+
+        /* Medium devices (tablets, 768px and up) */
+        @media (min-width: 768px) {
+    
+        }
+
+         /* Large devices (desktops, 992px and up) */
+        @media (min-width: 992px) {
+        
+        }
+
+        /* X-Large devices (large desktops, 1200px and up) */
+        @media (min-width: 1200px) {
+        
+        }
+
+        /* Large devices (larger desktops, 1400px and up) */
+        @media (min-width: 1400px) {
+        
+        }
+    </style>
 </footer>

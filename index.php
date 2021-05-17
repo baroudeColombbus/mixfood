11038de056ac997f737369cff0878340ea6c4882
LadyTank <69342492+LadyTank@users.noreply.github.com>
2021-05-17 16:51:07 +0200

    indentation

--- index.php
+++ index.php
@@ -10,9 +10,11 @@
 
     <!-- Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-wEmeIV1mKuiNpC+IOBjI7aAzPcEZeedi5yW5f2yOq55WWLwNGmvvx4Um1vskeMj0" crossorigin="anonymous">
+    <script src="https://kit.fontawesome.com/b41dcf0f5f.js" crossorigin="anonymous"></script>
+
 
     <!-- normalize -->
-    <link rel="stylesheet" href="scss/normalize.css">
+    <!-- <link rel="stylesheet" href="scss/normalize.css"> -->
 
     <!-- Les styles -->
     <link rel="stylesheet" href="scss/style.css">
@@ -25,9 +27,9 @@
     <title>MIXFOOD</title>
 </head>
 
-<body class="">
+<body class="bg-dark">
     <?php
-    include 'inc/navbar.php';
+    require_once 'inc/navbar.php';
     include 'inc/diaporama.php';
     ?>
     <div class="container">

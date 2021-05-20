7c6d520903f8c2e06e6927d9182ede91ac26d6cd
LadyTank <69342492+LadyTank@users.noreply.github.com>
2021-05-20 12:43:13 +0200

    acces admin

--- admin/manage_utilisateur_client.php
+++ admin/manage_utilisateur_client.php
@@ -4,63 +4,63 @@
 
 	<?php
 
-  require_once '../inc/navbar.php';
-  require_once '../inc/init.php';
-  require_once '../inc/functions.php';
+	require_once '../inc/navbar.php';
+	require_once '../inc/init.php';
+	require_once '../inc/functions.php';
 
-  $utilisateur = executeRequete(" SELECT * FROM utilisateur");
-  ?>
+	$utilisateur = executeRequete(" SELECT * FROM utilisateur");
+	?>
 
 	<div class="row my-5">
-	  <div class="col-sm-12 col-md-10">
-	    <h2 class="mb-4">Nos utilisateurs:</h2>
+		<div class="col-sm-12 col-md-10">
+			<h2 class="mb-4">Nos utilisateurs:</h2>
 
-	    <?php
+			<?php
 
 
-      $requete = $pdoMIXFOOD->query(" SELECT * FROM utilisateur ORDER BY id_utilisateur DESC"); ?>
+			$requete = $pdoMIXFOOD->query(" SELECT * FROM utilisateur ORDER BY id_utilisateur DESC"); ?>
 
-	    <table class="table table-light table-striped">
-	      <thead>
-	        <tr>
-	          <th scope="col">ID</th>
-	          <th scope="col">Prénom</th>
-	          <th scope="col">Nom</th>
-	          <th scope="col">Email</th>
-	          <th scope="col">Téléphone</th>
-	          <th scope="col">Mot de passe</th>
-	          <th scope="col">Adresse</th>
-	          <th scope="col">Ville</th>
-	          <th scope="col">Code postal</th>
-	          <th scope="col">Statut</th>
-	          <th scope="col">En savoir plus</th>
-	        </tr>
-	      </thead>
+			<table class="table table-light table-striped">
+				<thead>
+					<tr>
+						<th scope="col">ID</th>
+						<th scope="col">Prénom</th>
+						<th scope="col">Nom</th>
+						<th scope="col">Email</th>
+						<th scope="col">Téléphone</th>
+						<th scope="col">Mot de passe</th>
+						<th scope="col">Adresse</th>
+						<th scope="col">Ville</th>
+						<th scope="col">Code postal</th>
+						<th scope="col">Statut</th>
+						<th scope="col">En savoir plus</th>
+					</tr>
+				</thead>
 
-	      <?php
+				<?php
 
-        foreach ($requete as $user) {
-          echo "<tr>";
-          echo "<td>" . $user['id_utilisateur'] . "</td>";
-          echo "<td>" . $user['prenom'] . "</td>";
-          echo "<td>" . $user['nom'] . " </td>";
-          echo "<td>" . $user['email'] . " </td>";
-          echo "<td>" . $user['telephone'] . " </td>";
-          echo "<td>" . $user['mot_de_passe'] . " </td>";
-          echo "<td>" . $user['adresse'] . " </td>";
-          echo "<td>" . $user['ville'] . " </td>";
-          echo "<td>" . $user['code_postal'] . " </td>";
+				foreach ($requete as $user) {
+					echo "<tr>";
+					echo "<td>" . $user['id_utilisateur'] . "</td>";
+					echo "<td>" . $user['prenom'] . "</td>";
+					echo "<td>" . $user['nom'] . " </td>";
+					echo "<td>" . $user['email'] . " </td>";
+					echo "<td>" . $user['telephone'] . " </td>";
+					echo "<td>" . $user['mot_de_passe'] . " </td>";
+					echo "<td>" . $user['adresse'] . " </td>";
+					echo "<td>" . $user['ville'] . " </td>";
+					echo "<td>" . $user['code_postal'] . " </td>";
 
-          if ($user['statut_utilisateur'] == "admin") {
-            echo "<td class=\"text-danger\">Administrateur</td>";
-          } else {
-            echo "<td class=\"text-success\">Client</td>";
-          }
+					if ($user['statut_utilisateur'] == "admin") {
+						echo "<td class=\"text-danger\">Administrateur</td>";
+					} else {
+						echo "<td class=\"text-success\">Client</td>";
+					}
 
-          echo "<td><a href=\"consulter_une_annonce.php?id=" . $user['id_utilisateur'] . "\">En savoir plus </a></td>";
-          echo "</tr>";
-        }
-        ?>
-	    </table>
+					echo "<td><a href=\"consulter_une_annonce.php?id=" . $user['id_utilisateur'] . "\">En savoir plus </a></td>";
+					echo "</tr>";
+				}
+				?>
+			</table>
 
-	  </div><!-- fin col -->
+		</div><!-- fin col -->
--- contact.php
+++ contact.php
@@ -27,9 +27,9 @@
     include 'inc/navbar.php';
     ?>
 
-    <div class="container m-5 p-4">
+    <div class="container m-5 p-4 mx-auto">
         <h3 class="text-center">Contact</h3>
-        <p class="text-center"><em>Parce que votre avis nous intéresse</em></p>
+        <p class="text-center mb-5"><em>Parce que votre avis nous intéresse</em></p>
         <div class="row">
             <div class="col-md-4 col-sm-12">
                 <p>Laissez nous un commentaire</p>
@@ -40,21 +40,19 @@
             <div class="col-md-8 col-sm-12">
                 <div class="row">
                     <div class="col-xs-12 col-sm-12 col-lg-6 form-group mb-4">
-                        <input class="form-control" id="name" name="name" placeholder="Name" type="text" required>
+                        <input class="form-control" id="name" name="name" placeholder="Votre nom" type="text" required>
                     </div>
                     <div class="col-xs-12 col-sm-12 col-lg-6 form-group mb-4">
-                        <input class="form-control" id="email" name="email" placeholder="Email" type="email" required>
+                        <input class="form-control" id="email" name="email" placeholder="Votre email" type="email" required>
                     </div>
                 </div>
-                <textarea class="form-control mb-4" id="comments" name="comments" placeholder="Comment" rows="5"></textarea>
-                <div class="row bg-warning">
-                    <div class="col-md-12 form-group">
-                        <button class="btn pull-right" type="submit">Send</button>
-                    </div>
+                <textarea class="form-control mb-4" id="comments" name="comments" placeholder="Votre message" rows="5"></textarea>
+                <div class="row">
+                    <button class="btn btn-warning mr-1 mb-1 mx-auto boutonNav col-sm-3" id="btn-envoyer">Envoyer</button>
                 </div>
             </div>
-        </div> <!-- fin de container-->
-    </div>
+        </div> <!-- fin de row-->
+    </div> <!-- fin de container-->
     <?php
     include 'inc/footer.php';
     ?>

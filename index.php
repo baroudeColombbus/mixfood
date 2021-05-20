44dbad7a8ea7c3419fc64250b2ae36e1fc527f27
LadyTank <69342492+LadyTank@users.noreply.github.com>
2021-05-20 10:39:24 +0200

    head

--- index.php
+++ index.php
@@ -8,15 +8,15 @@
 
     <!-- Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-wEmeIV1mKuiNpC+IOBjI7aAzPcEZeedi5yW5f2yOq55WWLwNGmvvx4Um1vskeMj0" crossorigin="anonymous">
+
+    <!-- Les icônes-->
     <script src="https://kit.fontawesome.com/b41dcf0f5f.js" crossorigin="anonymous"></script>
 
-
     <!-- normalize -->
-    <!-- <link rel="stylesheet" href="scss/normalize.css"> -->
+    <link rel="stylesheet" href="scss/normalize.css">
 
     <!-- Les styles -->
     <link rel="stylesheet" href="scss/style.css">
-
 
     <!-- Les typographies -->
     <link rel="preconnect" href="https://fonts.gstatic.com">

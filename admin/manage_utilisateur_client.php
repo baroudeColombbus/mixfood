205cd66a0cbf27fbe2728653ffb48f1d2b6fc2f1
baroudeColombbus <69351692+baroudeColombbus@users.noreply.github.com>
2021-05-20 12:59:41 +0200

    Update manage_utilisateur_client.php

--- admin/manage_utilisateur_client.php
+++ admin/manage_utilisateur_client.php
@@ -12,7 +12,7 @@ $utilisateur = executeRequete(" SELECT * FROM utilisateur");
 		<h2 class="mb-4">Nos utilisateurs:</h2>
 
 		<?php
-		$requete = $pdoMIXFOOD->query(" SELECT * FROM utilisateur ORDER BY id_utilisateur DESC"); ?>
+		$requete = $pdoSITE->query(" SELECT * FROM utilisateur ORDER BY id_utilisateur DESC"); ?>
 
 		<table class="table table-light table-striped">
 			<thead>

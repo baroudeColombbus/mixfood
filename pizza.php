b4c8b4e1fce36f1141ad4abf6c09191452dbddb3
LadyTank <69342492+LadyTank@users.noreply.github.com>
2021-05-25 19:05:10 +0200

    id différent pour chaque accordion + passage php pour futur info

--- pizza.php
+++ pizza.php
@@ -11,16 +11,27 @@ include_once 'inc/haut.php';
         <div class="col-12 col-md-6 col-lg-4 mb-3">
             <img src="img/pizza-napolitaine-300.jpg" alt="pizza" class="img-curvy img-thumbnail" oneclick="" style="background-color:#28a745; border-color:#28a745;">
             <!-- accordeon -->
-            <div class="accordion accordion-flush my-2 mx-auto" id="accordion1" style="width:50%">
+            <div class="accordion accordion-flush my-2 mx-auto" id="accordionP1" style="width:50%">
                 <div class="accordion-item bg-success">
-                    <h2 class="accordion-header" id="flush-head-accordion">
-                        <button class="accordion-button collapsed btn btn-success" type="button" data-bs-toggle="collapse" data-bs-target="#flush-accordion1" aria-expanded="false" aria-controls="flush-accordion1" style="background-color: #0a5846ab; color: #fff">
+                    <h2 class="accordion-header" id="flush-head-accordionP">
+                        <button class="accordion-button collapsed btn btn-success" type="button" data-bs-toggle="collapse" data-bs-target="#flush-accordionP1" aria-expanded="false" aria-controls="flush-accordionP1" style="background-color: #0a5846ab; color: #fff">
                             Napolitaine 9.99€
+
+                            <?php
+
+                            ?>
+
                         </button>
                     </h2>
-                    <div id="flush-accordion1" class="accordion-collapse collapse bg-success activeVert" aria-labelledby="flush-head-accordion" data-bs-parent="#accordion1">
+                    <div id="flush-accordionP1" class="accordion-collapse collapse bg-success activeVert" aria-labelledby="flush-head-accordionP" data-bs-parent="#accordionP1">
                         <div class="accordion-body">
-                            <p> Tomates fraîches, fromage, basilic</p>
+                            <p> Tomates fraîches, fromage, basilic
+
+                                <?php
+
+                                ?>
+
+                            </p>
                         </div>
                     </div>
                 </div>
@@ -30,16 +41,27 @@ include_once 'inc/haut.php';
         <div class="col-12 col-md-6 col-lg-4 mb-3">
             <img src="img/pizza-300.jpg" alt="pizza" class="img-curvy img-thumbnail" style="background-color:#28a745; border-color:#28a745;">
             <!-- accordeon -->
-            <div class="accordion accordion-flush my-2 mx-auto" id="accordion2" style="width:50%">
+            <div class="accordion accordion-flush my-2 mx-auto" id="accordionP2" style="width:50%">
                 <div class="accordion-item bg-success">
-                    <h2 class="accordion-header" id="flush-head-accordion2">
-                        <button class="accordion-button collapsed btn btn-success" type="button" data-bs-toggle="collapse" data-bs-target="#flush-accordion2" aria-expanded="false" aria-controls="flush-accordion2" style="background-color: #0a5846ab; color: #fff">
+                    <h2 class="accordion-header" id="flush-head-accordionP2">
+                        <button class="accordion-button collapsed btn btn-success" type="button" data-bs-toggle="collapse" data-bs-target="#flush-accordionP2" aria-expanded="false" aria-controls="flush-accordionP2" style="background-color: #0a5846ab; color: #fff">
                             Reine 11.98€
+
+                            <?php
+
+                            ?>
+
                         </button>
                     </h2>
-                    <div id="flush-accordion2" class="accordion-collapse collapse bg-success" aria-labelledby="flush-head-accordion2" data-bs-parent="#accordion2">
+                    <div id="flush-accordionP2" class="accordion-collapse collapse bg-success" aria-labelledby="flush-head-accordionP2" data-bs-parent="#accordionP2">
                         <div class="accordion-body">
-                            <p> Tomates fraîches, fromage, champignon de Paris, basilic</p>
+                            <p> Tomates fraîches, fromage, champignon de Paris, basilic
+
+                                <?php
+
+                                ?>
+
+                            </p>
                         </div>
                     </div>
                 </div>
@@ -49,16 +71,27 @@ include_once 'inc/haut.php';
         <div class="col-12 col-md-6 col-lg-4 mb-3">
             <img src="img/pizza3-300.jpg" alt="pizza" class="img-curvy img-thumbnail" style="background-color:#28a745; border-color:#28a745;">
             <!-- accordeon -->
-            <div class="accordion accordion-flush my-2 mx-auto" id="accordion3" style="width:50%">
+            <div class="accordion accordion-flush my-2 mx-auto" id="accordionP3" style="width:50%">
                 <div class="accordion-item bg-success">
-                    <h2 class="accordion-header" id="flush-head-accordion3">
-                        <button class="accordion-button collapsed btn" type="button" data-bs-toggle="collapse" data-bs-target="#flush-accordion3" aria-expanded="false" aria-controls="flush-collapseOne" style="background-color: #0a5846ab; color: #fff">
+                    <h2 class="accordion-header" id="flush-head-accordionP3">
+                        <button class="accordion-button collapsed btn" type="button" data-bs-toggle="collapse" data-bs-target="#flush-accordionP3" aria-expanded="false" aria-controls="flush-accordionP3" style="background-color: #0a5846ab; color: #fff">
                             Reine du Sud 12.56€
+
+                            <?php
+
+                            ?>
+
                         </button>
                     </h2>
-                    <div id="flush-accordion3" class="accordion-collapse collapse bg-success" aria-labelledby="flush-head-accordion3" data-bs-parent="#accordion3">
+                    <div id="flush-accordionP3" class="accordion-collapse collapse bg-success" aria-labelledby="flush-head-accordionP3" data-bs-parent="#accordionP3">
                         <div class="accordion-body">
-                            <p> Tomates fraîches, fromage, champignon de Paris, poivrons, basilic</p>
+                            <p> Tomates fraîches, fromage, champignon de Paris, poivrons, basilic
+
+                                <?php
+
+                                ?>
+
+                            </p>
                         </div>
                     </div>
                 </div>
@@ -68,16 +101,27 @@ include_once 'inc/haut.php';
         <div class="col-12 col-md-6 col-lg-4 mb-3">
             <img src="img/pizza4-300.jpg" alt="pizza" class="img-curvy img-thumbnail" style="background-color:#28a745; border-color:#28a745;">
             <!-- accordeon -->
-            <div class="accordion accordion-flush my-2 mx-auto" id="accordion4" style="width:50%">
+            <div class="accordion accordion-flush my-2 mx-auto" id="accordionP4" style="width:50%">
                 <div class="accordion-item bg-success">
-                    <h2 class="accordion-header" id="flush-head-accordion4">
-                        <button class="accordion-button collapsed btn" type="button" data-bs-toggle="collapse" data-bs-target="#flush-accordion4" aria-expanded="false" aria-controls="flush-accordion4" style="background-color: #0a5846ab; color: #fff">
+                    <h2 class="accordion-header" id="flush-head-accordionP4">
+                        <button class="accordion-button collapsed btn" type="button" data-bs-toggle="collapse" data-bs-target="#flush-accordionP4" aria-expanded="false" aria-controls="flush-accordionP4" style="background-color: #0a5846ab; color: #fff">
                             4 fromages 8.99€
+
+                            <?php
+
+                            ?>
+
                         </button>
                     </h2>
-                    <div id="flush-accordion4" class="accordion-collapse collapse bg-success" aria-labelledby="flush-head-accordion4" data-bs-parent="#accordion4">
+                    <div id="flush-accordionP4" class="accordion-collapse collapse bg-success" aria-labelledby="flush-head-accordionP4" data-bs-parent="#accordionP4">
                         <div class="accordion-body">
-                            <p> Tomates fraîches, mozarella, parmesan, chèvre, tome</p>
+                            <p> Tomates fraîches, mozarella, parmesan, chèvre, tome
+
+                                <?php
+
+                                ?>
+
+                            </p>
                         </div>
                     </div>
                 </div>
@@ -87,16 +131,27 @@ include_once 'inc/haut.php';
         <div class="col-12 col-md-6 col-lg-4 mb-3">
             <img src="img/pizza5-300.jpg" alt="pizza" class="img-curvy img-thumbnail" style="background-color:#28a745; border-color:#28a745;">
             <!-- accordeon -->
-            <div class="accordion accordion-flush my-2 mx-auto" id="accordion5" style="width:50%">
+            <div class="accordion accordion-flush my-2 mx-auto" id="accordionP5" style="width:50%">
                 <div class="accordion-item bg-success">
-                    <h2 class="accordion-header" id="flush-head-accordion5">
-                        <button class="accordion-button collapsed btn" type="button" data-bs-toggle="collapse" data-bs-target="#flush-accordion5" aria-expanded="false" aria-controls="flush-accordion5" style="background-color: #0a5846ab; color: #fff">
+                    <h2 class="accordion-header" id="flush-head-accordionP5">
+                        <button class="accordion-button collapsed btn" type="button" data-bs-toggle="collapse" data-bs-target="#flush-accordionP5" aria-expanded="false" aria-controls="flush-accordionP5" style="background-color: #0a5846ab; color: #fff">
                             Chorizo Party 15.56€
+
+                            <?php
+
+                            ?>
+
                         </button>
                     </h2>
-                    <div id="flush-accordion5" class="accordion-collapse collapse bg-success" aria-labelledby="flush-head-accordion5" data-bs-parent="#accordion5">
+                    <div id="flush-accordionP5" class="accordion-collapse collapse bg-success" aria-labelledby="flush-head-accordionP5" data-bs-parent="#accordionP5">
                         <div class="accordion-body">
-                            <p> Tomates fraîches, fromage, chorizo à partager</p>
+                            <p> Tomates fraîches, fromage, chorizo à partager
+
+                                <?php
+
+                                ?>
+
+                            </p>
                         </div>
                     </div>
                 </div>
@@ -106,16 +161,27 @@ include_once 'inc/haut.php';
         <div class="col-12 col-md-6 col-lg-4 ">
             <img src="img/pizza6-300.jpg" alt="pizza" class="img-curvy img-thumbnail" style="background-color:#28a745; border-color:#28a745;">
             <!-- accordeon -->
-            <div class="accordion accordion-flush my-2 mx-auto" id="accordion6" style="width:50%">
+            <div class="accordion accordion-flush my-2 mx-auto" id="accordionP6" style="width:50%">
                 <div class="accordion-item bg-success">
-                    <h2 class="accordion-header" id="flush-head-accordion6">
-                        <button class="accordion-button collapsed btn" type="button" data-bs-toggle="collapse" data-bs-target="#flush-accordion6" aria-expanded="false" aria-controls="flush-accordion6" style="background-color: #0a5846ab; color: #fff">
+                    <h2 class="accordion-header" id="flush-head-accordionP6">
+                        <button class="accordion-button collapsed btn" type="button" data-bs-toggle="collapse" data-bs-target="#flush-accordionP6" aria-expanded="false" aria-controls="flush-accordionP6" style="background-color: #0a5846ab; color: #fff">
                             Calzone 11.52€
+
+                            <?php
+
+                            ?>
+
                         </button>
                     </h2>
-                    <div id="flush-accordion6" class="accordion-collapse collapse bg-success" aria-labelledby="flush-head-accordion6" data-bs-parent="#accordion6">
+                    <div id="flush-accordionP6" class="accordion-collapse collapse bg-success" aria-labelledby="flush-head-accordionP6" data-bs-parent="#accordionP6">
                         <div class="accordion-body">
-                            <p> Tomates fraîches, fromage, basilic</p>
+                            <p> Tomates fraîches, fromage, basilic
+
+                                <?php
+
+                                ?>
+
+                            </p>
                         </div>
                     </div>
                 </div>

fff7083a6222ca0254289fa0dd18b833498c5eab
baroudeColombbus <69351692+baroudeColombbus@users.noreply.github.com>
2021-05-25 15:26:40 +0200

    suite

--- admin/gestion_categorie.php
+++ admin/gestion_categorie.php
@@ -8,17 +8,33 @@ require_once 'inc/haut.php';
 ?>
 <!-- menu principal -->
 <div class="container m-auto">
+    <?php 
+    // on active la session de suppression de category
+    if(isset($_SESSION['supprimer'])){
+        echo $_SESSION['supprimer'];
+        unset($_SESSION['supprimer']);
+
+    }
+
+    // on active la session d'ajout de category
+    if(isset($_SESSION['ajouter_cat'])){
+        echo $_SESSION['ajouter_cat'];
+        unset($_SESSION['ajouter_cat']);
+    }
+
+
+    ?>
 
     <!-- button to ad admin  -->
     <a href="<?php echo SITEURL; ?>admin/ajouter_categorie.php" class="btn btn-primary my-2">Ajouter une categorie</a>
-    <!--  -->
+   
     <table class="table table-striped mx-auto">
         <thead class="table-dark">
             <tr>
                 <th>ID</th>
                 <th>Categorie</th>
                 <th>Image</th>
-                <th>en_vedette</th>
+                <th>Category vedette</th>
                 <th>Disponible</th>
                 <th>Modifier</th>
                 <th>Supprimer</th>
@@ -51,7 +67,6 @@ require_once 'inc/haut.php';
 
 
 
-
             foreach ($result as $row) {
 
                 $nom_image = $row['nom_image'];
@@ -67,6 +82,7 @@ require_once 'inc/haut.php';
 
                     if ($nom_image != '') {
                     ?>
+                    <!-- mettre l'image en php et reduire ce passage html -->
                         <img src="<?php echo SITEURL; ?>img/categorie/<?php echo $nom_image; ?>" width="100px">
 
 
@@ -75,21 +91,16 @@ require_once 'inc/haut.php';
                         ?>
                 </td>
             <?php
-                        echo "<td>" . $row['en_vedette'] . "</td>";
-                        echo "<td>" . $row['disponible'] . "</td>";
-                        echo "<td> <a href=\"update_category.php?id=" . $row['id_categorie'] . "\" class=\"btn btn-warning \">Modifier la gategorie</a></td>";
-                        echo "<td> <a href=\"supprimer_.php?id=" . $row['id_categorie'] . "\" class=\"btn btn-info \">Supprimer la gategorie</a></td>";
-                        echo    "<tr>";
+                       
                     }
             ?>
 
-            ?>
             </td>
         <?php
                 echo "<td>" . $row['en_vedette'] . "</td>";
                 echo "<td>" . $row['disponible'] . "</td>";
-                echo "<td> <a href=\"modifier_category.php?id=" . $row['id_categorie'] . "\" class=\"btn btn-warning \">Modifier la gategorie</a></td>";
-                echo "<td> <a href=\"supprimer_category.php?id=" . $row['id_categorie'] . "\" class=\"btn btn-info \">Supprimer la gategorie</a></td>";
+                echo "<td> <a href=\"modifier_categorie.php?id=" . $row['id_categorie'] . "\" class=\"btn btn-warning \">Modifier la gategorie</a></td>";
+                echo "<td> <a href=\"supprimer_categorie.php?id=" . $row['id_categorie'] . "\" class=\"btn btn-info \">Supprimer la gategorie</a></td>";
                 echo    "<tr>";
             }
         ?>
